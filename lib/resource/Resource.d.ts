a012c22fae87c8851fe8e06af22042347ebcf24a
Jens Struwe <jens@struwe.net>
2018-02-27 15:16:11 +0100

    add Resource

---
+++ lib/resource/Resource.d.ts
@@ -0,0 +1,35 @@
+import Model from '../model/Model';
+export default class Resource {
+    constructor(...params: any[]);
+    init(params?: any): void;
+    /**
+     * Resource Config
+     */
+    getListType(): string;
+    /**
+     * Since Search or Todos resources return lists of mixed items
+     * we need to decide what resource cache key is to be
+     * used based on the actual item's type.
+     * @see Search or Todos resources
+     */
+    getItemType(json?: any): string;
+    getItemId(json: any): any;
+    getItemJson(json: any): any;
+    getItemModel(json: any): typeof Model;
+    createItem(json: any): Model;
+    transformList(items: any): void;
+    /**
+     * Api Hooks
+     */
+    itemAdded(item: any): void;
+    itemDeleted(item: any): void;
+    itemSaved(itemOld: any, item: any): void;
+    /**
+     * Resource Cache Access
+     */
+    cachePurgeList(key: any, url: any): void;
+    cachePurgeRelation(relation: any): void;
+    cachePurgeItem(key: any, id: any): void;
+    cacheGetAllLists(key: any): any;
+    findCachedItem(key: any, id: any): any;
+}
---
+++ src/resource/Resource.ts
@@ -0,0 +1,102 @@
+import resourceCache from '../cache/ResourceCache'
+import Model from '../model/Model'
+
+export default class Resource {
+  constructor (...params) {
+    this.init(...params)
+  }
+
+  public init (params?) {
+    // hook into
+  }
+
+  /**
+   * Resource Config
+   */
+
+  public getListType () {
+    return this.getItemType()
+  }
+
+  /**
+   * Since Search or Todos resources return lists of mixed items
+   * we need to decide what resource cache key is to be
+   * used based on the actual item's type.
+   * @see Search or Todos resources
+   */
+  public getItemType (json?) {
+    return this.getItemModel(json).type
+  }
+
+  public getItemId (json) {
+    return this.getItemJson(json).id
+  }
+
+  public getItemJson (json) {
+    return json
+  }
+
+  public getItemModel (json): typeof Model {
+    // hook into
+    return Model
+  }
+
+  // creates a new model based on the given json response
+  // @see Todos or Search
+  public createItem (json) {
+    const item = new (this.getItemModel(json))()
+    item.id = this.getItemId(json)
+    return item
+  }
+
+  // transforms the given list prior to caching
+  // useful to create a hierachical list from a flat list
+  // e.g. for cateories
+  public transformList (items) {
+    // hook into
+  }
+
+  /**
+   * Api Hooks
+   */
+
+  // called after an item has been added
+  // to enable custom resource cache treatment
+  public itemAdded (item) {
+    // hook into
+  }
+
+  // called after an item has been deleted
+  // to enable custom resource cache treatment
+  public itemDeleted (item) {
+    // hook into
+  }
+
+  public itemSaved (itemOld, item) {
+    // hook into
+  }
+
+  /**
+   * Resource Cache Access
+   */
+
+  public cachePurgeList (key, url) {
+    resourceCache.purgeList(key, url)
+  }
+
+  public cachePurgeRelation (relation) {
+    relation.purgeFromCacheAndMarkInvalid()
+  }
+
+  public cachePurgeItem (key, id) {
+    resourceCache.purgeItem(key, id)
+  }
+
+  public cacheGetAllLists (key) {
+    return resourceCache.getCache(key).lists
+  }
+
+  public findCachedItem (key, id) {
+    return resourceCache.getItem(key, id)
+  }
+}
