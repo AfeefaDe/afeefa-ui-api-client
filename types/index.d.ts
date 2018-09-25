4a7d6e4677d5bbac34549888503c60fc18fcf38d
Jens Struwe <jens@struwe.net>
2018-09-25 21:42:38 +0200

    add types

---
+++ types/index.d.ts
@@ -0,0 +1,12 @@
+import Api from './api/Api';
+import ApiError from './api/ApiError';
+import LoadingState from './api/LoadingState';
+import ResourceCache from './cache/ResourceCache';
+import App from './model/App';
+import DataTypes from './model/DataTypes';
+import Model from './model/Model';
+import PlainJson from './model/PlainJson';
+import Registry from './model/Registry';
+import Relation from './model/Relation';
+import Resource from './resource/Resource';
+export { Api, ApiError, LoadingState, ResourceCache, App, DataTypes, Model, PlainJson, Registry, Relation, Resource };
