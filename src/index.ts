da20ccccb9a3a1ea024df44baf99366cf9b1582e
Jens Struwe <jens@struwe.net>
2018-10-09 00:49:05 +0200

    namespaces + compile to es5

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,7 @@
-import Api from './api/Api'
-import ApiError from './api/ApiError'
-import LoadingState from './api/LoadingState'
-import ResourceCache from './cache/ResourceCache'
-import App from './model/App'
-import DataTypes from './model/DataTypes'
-import Model from './model/Model'
-import PlainJson from './model/PlainJson'
-import Registry from './model/Registry'
-import Relation from './model/Relation'
-import Resource from './resource/Resource'
+import * as api from './api'
+import * as model from './model'
 
 export {
-  Api,
-  ApiError,
-  LoadingState,
-  ResourceCache,
-  App,
-  DataTypes,
-  Model,
-  PlainJson,
-  Registry,
-  Relation,
-  Resource
+  model,
+  api
 }
